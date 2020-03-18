89053b427b69ab3251198c3fca138ca169cd6448
天气晴 <@LAPTOP-CCRVRILF>
2020-03-18 22:10:15 +0800

    homework4

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
+
+namespace Homework4
+{
+    class Program
+    {
+        // 链表节点
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        //泛型链表类
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get { return head; }
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+                for (Node<T> x = head; x != null; x = x.Next)
+                    action(x.Data);
+            }
+        }
+
+       
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+                list.Add(i);
+            int sum = 0;int max = int.MinValue;int min = int.MaxValue;
+            list.ForEach(t => sum += t);
+            list.ForEach(t => max = t > max ? t : max);
+            list.ForEach(t =>min=t<min? t : min);
+            Console.WriteLine($"the maxvalue of list is:{max}");
+            Console.WriteLine($"the minvalue of list is:{min}");
+            Console.WriteLine($"the sum of list is:{sum}");
+
+        }
+    }
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//2、使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台显示提示信息。
+namespace Homework4_2
+{
+    public delegate void Tick_Alarm_Handler(object sender, EventArgs args);
+    public class ClockEvent
+    {
+        public event Tick_Alarm_Handler OnTick;//定义tick事件
+        public event Tick_Alarm_Handler OnAlarm;//定义alarm事件
+
+        private DateTime currenttime;
+        public DateTime Curenttime
+        {
+            get { return currenttime; }
+            set
+            {
+                currenttime = value;
+                if (currenttime.Second == 0 || currenttime.Second == 30)  //Clock设置每整分或半分时刻alarm
+                    this.OnAlarm(this, new EventArgs());
+                else this.OnTick(this, new EventArgs());                  //其他时刻tick
+            }
+        }
+        public void TimeChanged(EventArgs args)
+        {
+            if (OnTick != null)
+                this.OnTick(this, args);
+        }
+    }
+    public class Clock
+    {
+        public ClockEvent clockevent = new ClockEvent();
+
+        public Clock()                                                  //订阅事件
+        {
+            clockevent.OnTick += Clock_Tick;
+            clockevent.OnAlarm += Clock_Alarm;
+        }
+
+        public void Clock_Tick(object sender, EventArgs args)           //Tick处理方法
+        {
+            Console.WriteLine($"{clockevent.Curenttime},Tick");
+        }
+        public void Clock_Alarm(object sender, EventArgs args)          //Alarm处理方法
+        {
+            Console.WriteLine($"{clockevent.Curenttime},Alarm!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock1 = new Homework4_2.Clock();
+            while (true)
+            { 
+                clock1.clockevent.Curenttime = DateTime.Now;
+                System.Threading.Thread.Sleep(1000);
+            }
+
+
+        }
+    }
+}
